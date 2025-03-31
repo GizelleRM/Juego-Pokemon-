58964948e6795927bffceb21f5c05ed75fdd5bd8
aneli1 <148299361+aneli1@users.noreply.github.com>
2025-03-30 20:16:05 -0600

    Anelí Arce: agregando test
    Agregue los test de cada función, uno por nivel.

--- nivel_2/tests.py
+++ nivel_2/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from Users.models import Users  # importa tu modelo de usuario
+class Nivel2Tests(TestCase):
 
-# Create your tests here.
+    def setUp(self):
+        # Creamos un usuario de prueba para usar en los tests
+        self.usuario = Users.objects.create(
+            nickName="prueba123",
+            id_instructor=1,
+            id_mascota=1,
+            nivel=2
+        )
+        self.client = Client()  # Cliente de prueba para simular peticiones
+
+    def test_acceso_a_vista_nivel_2(self):
+        # Llamamos a la URL con el nickname creado
+        url = reverse('nivel_2', args=[self.usuario.nickName])
+        response = self.client.get(url)
+
+        # Comprobamos que la respuesta fue exitosa (HTTP 200)
+        self.assertEqual(response.status_code, 200)
+
+        # Comprobamos que el template correcto fue usado
+        self.assertTemplateUsed(response, 'nivel_2/nivel-2.html')
+
+        # Comprobamos que el contexto contiene el nickname
+        self.assertContains(response, self.usuario.nickName)
